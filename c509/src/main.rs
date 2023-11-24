1b36f07c3eb7d1eb3e50ade137638c7a4cc3f796
Joel H <joel.hoglund@ri.se>
2023-11-24 03:40:01 +0100

    update in progress

--- c509/src/main.rs
+++ c509/src/main.rs
@@ -691,7 +691,7 @@ pub mod der {
         }
     }
 
-    // C509 Certificate Extensions Registry
+    // C509 Certificate Extensions Registry *updated 2023-11 */
     pub const EXT_SUBJECT_KEY_ID: u16 = 1;
     pub const EXT_KEY_USAGE: u16 = 2;
     pub const EXT_SUBJECT_ALT_NAME: u16 = 3;
@@ -702,22 +702,67 @@ pub mod der {
     pub const EXT_EXT_KEY_USAGE: u16 = 8;
     pub const EXT_AUTH_INFO: u16 = 9;
     pub const EXT_SCT_LIST: u16 = 10;
-
+    
+    pub const EXT_SUBJECT_DIRECTORY_ATTR: u16 = 24; //0x09
+    pub const EXT_ISSUER_ALT_NAME: u16 = 25;        //0x12
+    pub const EXT_NAME_CONSTRAINTS: u16 = 26;       //0x1E
+    pub const EXT_POLICY_MAPPINGS: u16 = 27;        //21
+    pub const EXT_POLICY_CONSTRAINTS: u16 = 28;     //24
+    pub const EXT_FRESHEST_CRL: u16 = 29;           //2e
+    pub const EXT_INHIBIT_ANYPOLICY: u16 = 30;      //36
+    pub const EXT_SUBJECT_INFO_ACCESS: u16 = 31;    //5-0b
+    pub const EXT_IP_RESOURCES: u16 = 32;           //5-07
+    pub const EXT_AS_RESOURCES: u16 = 33;           //5-08
+    pub const EXT_IP_RESOURCES_V2: u16 = 34;        //5-1c
+    pub const EXT_AS_RESOURCES_V2: u16 = 35;        //5-1d
+    pub const EXT_BIOMETRIC_INFO: u16 = 36;         //5-02
+    pub const EXT_PRECERT_SIGNING_CERT: u16 = 37;   //4-04
+    pub const EXT_OCSP_NO_CHECK: u16 = 38;          //2B 06 01 05 05 07 30 01 05
+    pub const EXT_QUALIFIED_CERT_STATEMENTS: u16 = 39;//5-03
+    pub const EXT_S_MIME_CAPABILITIES: u16 = 40;    //2A 86 48 86 F7 0D 01 09 0F
+    pub const EXT_TLS_FEATURES: u16 = 41;           //5-18
+//    pub const EXT_SUBJECT_INFO_ACCESS: u16 = 42;    //5-0b
+    pub const EXT_CHALLENGE_PASSWORD: u16 = 255;
+    
     pub fn ext_map(oid: &[u8]) -> Option<u16> {
         match oid {
             [0x55, 0x1D, ..] => match oid[2] {
+                0x09 => Some(EXT_SUBJECT_DIRECTORY_ATTR),
                 0x0E => Some(EXT_SUBJECT_KEY_ID),
                 0x0F => Some(EXT_KEY_USAGE),
                 0x11 => Some(EXT_SUBJECT_ALT_NAME),
+                0x12 => Some(EXT_ISSUER_ALT_NAME),
                 0x13 => Some(EXT_BASIC_CONSTRAINTS),
+                0x1E => Some(EXT_NAME_CONSTRAINTS),
                 0x1F => Some(EXT_CRL_DIST_POINT),
                 0x20 => Some(EXT_CERT_POLICIES),
+                0x20 => Some(EXT_POLICY_MAPPINGS),
                 0x23 => Some(EXT_AUTH_KEY_ID),
+                0x24 => Some(EXT_POLICY_CONSTRAINTS),
                 0x25 => Some(EXT_EXT_KEY_USAGE),
+                0x2E => Some(EXT_FRESHEST_CRL),
+                0x36 => Some(EXT_INHIBIT_ANYPOLICY),
                 _ => None,
             },
-            [0x2B, 0x06, 0x01, 0x05, 0x05, 0x07, 0x01, 0x01] => Some(EXT_AUTH_INFO),
-            [0x2B, 0x06, 0x01, 0x04, 0x01, 0xD6, 0x79, 0x02, 0x04, 0x02] => Some(EXT_SCT_LIST),
+            [0x2B, 0x06, 0x01, 0x04, 0x01, 0xD6, 0x79, 0x02, 0x04, ..] => match oid[9] {
+                0x02 => Some(EXT_SCT_LIST),
+                0x04 => Some(EXT_PRECERT_SIGNING_CERT),
+                _ => None,
+            },
+            [0x2B, 0x06, 0x01, 0x05, 0x05, 0x07, 0x01, ..] => match oid[7] {
+                0x01 => Some(EXT_AUTH_INFO),
+                0x02 => Some(EXT_BIOMETRIC_INFO),
+                0x03 => Some(EXT_QUALIFIED_CERT_STATEMENTS),
+                0x07 => Some(EXT_IP_RESOURCES),
+                0x08 => Some(EXT_AS_RESOURCES),
+                0x0B => Some(EXT_SUBJECT_INFO_ACCESS),
+                0x18 => Some(EXT_TLS_FEATURES),
+                0x1C => Some(EXT_IP_RESOURCES_V2),
+                0x1D => Some(EXT_AS_RESOURCES_V2),
+                _ => None,
+            },
+           [0x2B, 0x06, 0x01, 0x05, 0x05, 0x07, 0x30, 0x01, 0x05] => Some(EXT_OCSP_NO_CHECK),
+           [0x2B, 0x86, 0x48, 0x86, 0xF7, 0x0D, 0x01, 0x09, 0x0F] => Some(EXT_S_MIME_CAPABILITIES),
             _ => None,
         }
     }
@@ -729,9 +774,22 @@ pub mod der {
     pub const EKU_EMAIL_PROT: u16 = 4;
     pub const EKU_TIME_STAMP: u16 = 8;
     pub const EKU_OCSP_SIGN: u16 = 9;
-
+    /* Added */
+    pub const EKU_ANY_EKU: u16 = 0;                       //55 1D 25 00
+    pub const EKU_KERBEROS_PKINIT_CLIENT_AUTH: u16 = 10;  //2B 06 01 05 02 03 04
+    pub const EKU_KERBEROS_PKINIT_KDC: u16 = 11;          //2B 06 01 05 02 03 05
+    pub const EKU_SSH_CLIENT: u16 = 12;                   //15
+    pub const EKU_SSH_SERVER: u16 = 13;                   //16
+    pub const EKU_BUNDLE_SECURITY: u16 = 14;              //23
+    
     pub fn eku_map(oid: &[u8]) -> Option<u16> {
         match oid {
+            [0x2B, 0x06, 0x01, 0x05, 0x02, 0x03, ..] => match oid[6] {
+            0x04 => Some(EKU_KERBEROS_PKINIT_CLIENT_AUTH),
+            0x05 => Some(EKU_KERBEROS_PKINIT_KDC),
+            _ => None,
+            },
+            
             [0x2B, 0x06, 0x01, 0x05, 0x05, 0x07, 0x03, ..] => match oid[7] {
                 0x01 => Some(EKU_TLS_SERVER),
                 0x02 => Some(EKU_TLS_CLIENT),
@@ -739,8 +797,12 @@ pub mod der {
                 0x04 => Some(EKU_EMAIL_PROT),
                 0x08 => Some(EKU_TIME_STAMP),
                 0x09 => Some(EKU_OCSP_SIGN),
+                0x15 => Some(EKU_SSH_CLIENT),
+                0x16 => Some(EKU_SSH_SERVER),
+                0x23 => Some(EKU_BUNDLE_SECURITY),
                 _ => None,
             },
+            [0x55, 0x1D, 0x25, 0x00] => Some(EKU_ANY_EKU),
             _ => None,
         }
     }
